64c367f6d11e24a233269b996ad65a574df00916
teostojkovski <teo.stojkovski@gmail.com>
2024-10-17 12:38:52 +0200

    Initial commit

---
+++ src/main/java/com/teo/OnlineFoodShopApplication.java
@@ -0,0 +1,13 @@
+package com.teo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodShopApplication.class, args);
+	}
+
+}
