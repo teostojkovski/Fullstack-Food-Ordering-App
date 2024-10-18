48d730744080de3b87c3e25c629200f2061d4f8d
teostojkovski <teo.stojkovski@gmail.com>
2024-10-18 20:20:17 +0200

    Repository commit

---
+++ src/main/java/com/teo/model/CartItem.java
@@ -0,0 +1,32 @@
+package com.teo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    private Cart cart;
+
+    @ManyToOne // many cartItems have the same food, multiple users will have the same food as an item
+    private Food food;
+
+    private int quantity;
+
+    private List<String> ingredients;
+
+    private Long totalPrice;
+}
