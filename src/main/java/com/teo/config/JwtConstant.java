b7eaebb51aea029c21cf556bbed90dc7fcb79832
teostojkovski <teo.stojkovski@gmail.com>
2024-10-19 02:57:53 +0200

    Security commit

---
+++ src/main/java/com/teo/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.teo.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="jkfdhajfakdfhajfdafjdahfjdhdajwhufwu";
+    public static final String JWT_HEADER="Authorization";
+}
