009c057507a0bb772df6f3d7bd57f42c9e968a0f
teostojkovski <teo.stojkovski@gmail.com>
2024-10-20 17:35:18 +0200

    Service commit

---
+++ src/main/java/com/teo/controller/AuthController.java
@@ -0,0 +1,10 @@
+package com.teo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    
+}
