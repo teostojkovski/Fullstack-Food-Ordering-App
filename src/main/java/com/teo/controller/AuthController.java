47bff7f3dc2113fc392c8842611a8262cfc04321
teostojkovski <teo.stojkovski@gmail.com>
2024-12-09 21:30:05 +0100

    09.12.2024

--- src/main/java/com/teo/controller/AuthController.java
+++ src/main/java/com/teo/controller/AuthController.java
@@ -2,22 +2,30 @@ package com.teo.controller;
 
 import com.teo.config.JwtProvider;
 import com.teo.model.Cart;
+import com.teo.model.USER_ROLE;
 import com.teo.model.User;
 import com.teo.repository.CartRepository;
 import com.teo.repository.UserRepository;
+import com.teo.request.LoginRequest;
 import com.teo.response.AuthResponse;
 import com.teo.service.CustomerUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/auth")
@@ -71,5 +79,31 @@ public class AuthController {
         return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
     }
 
-    public ResponseEntity<AuthResponse> signin(@RequestBody )
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signin(@RequestBody LoginRequest loginRequest){
+        String username = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+
+        Authentication authentication = authenticate(username, password);
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String role = authorities.isEmpty()?null:authorities.iterator().next().getAuthority();
+        String jwt=jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Successful registration");
+        authResponse.setRole(USER_ROLE.valueOf(role));
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customerUserDetailsService.loadUserByUsername(username);
+        if(userDetails==null){
+            throw new BadCredentialsException("invalid username.");
+        }
+        if(!passwordEncoder.matches(password,userDetails.getPassword())){
+            throw new BadCredentialsException("invalid password.");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
 }
--- src/main/java/com/teo/request/LoginRequest.java
+++ src/main/java/com/teo/request/LoginRequest.java
@@ -1,4 +1,9 @@
 package com.teo.request;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+    private String email;
+    private String password;
 }
